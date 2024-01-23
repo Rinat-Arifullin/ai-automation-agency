a308ec819598a1db6081b2626eb7dacea8634bda
arifullinrd <arifullinrd@lad24.ru>
2024-01-24 02:22:29 +0300

    api

--- utils/sendMessageToBot.ts
+++ utils/sendMessageToBot.ts
@@ -1,26 +1,24 @@
 import { IContactForm } from 'components/Main/ContactForm'
+import axios from 'axios'
+
+const options = (form: IContactForm) => ({
+    method: 'POST',
+    url: `https://api.telegram.org/bot${process.env.BOT_RECIVER_TOKEN}/sendMessage`,
+    headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+    },
+    data: {
+        text: `Name: ${form.name}\nPhone: ${form.phone}\nTelegram or Email: ${form.telegramOrEmail}\nDescription: ${form.description}`,
+        parse_mode: 'HTML',
+        chat_id: process.env.BOT_CHAT_ID,
+    },
+})
 
 export const sendMessageToBot = async (form: IContactForm) => {
-    const BOT_RECIVER_TOKEN = process.env.BOT_RECIVER_TOKEN
-    const BOT_CHAT_ID = process.env.BOT_CHAT_ID
-
-    if (!BOT_RECIVER_TOKEN || !BOT_CHAT_ID) {
-        console.error(
-            'Bot Receiver Token and/or Chat ID are not defined in the environment variables.'
-        )
-    }
-
-    const baseURL = `https://api.telegram.org/bot${BOT_RECIVER_TOKEN}/sendMessage?chat_id=${BOT_CHAT_ID}&text=`
-
-    let messageText = ''
-    messageText += `Name: ${form.name}\n`
-    messageText += `Phone: ${form.phone}\n`
-    messageText += `Telegram or Email: ${form.telegramOrEmail}\n`
-    messageText += `Description: ${form.description}\n`
-
     if (Object.values(form).every((el) => !el)) {
         return
     }
 
-    return await fetch(baseURL + messageText)
+    return await axios.request(options(form))
 }
